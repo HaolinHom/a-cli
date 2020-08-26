bca93f5f08ce32fe887942b7bb7e095849b14856
HaolinHom <haolinhom@gmail.com>
2020-08-26 17:44:57 +0800

    feat: package will install when dev command in debug mode;

--- commands/dev/index.js
+++ commands/dev/index.js
@@ -2,14 +2,19 @@ const { std } = require('wu-utils');
 const getCommandPath = require('../../utils/getCommandPath');
 const getContext = require('../../utils/getContext');
 const getArgs = require('../../utils/getArgs');
+const packageInstall = require('../../utils/packageInstall');
 
-async function dev() {
+async function dev(command) {
+  const isDebug = command.debug;
   const commandJsPath = getCommandPath();
   if (commandJsPath === false) {
     return;
   }
   const commandJs = require(commandJsPath);
   if (typeof commandJs === 'function') {
+    if (isDebug) {
+      packageInstall();
+    }
     // TODO: change to fork child_process
     commandJs(getContext(), getArgs());
   } else {
