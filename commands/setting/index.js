b36b279a3da02ca0ca9de66d723e42db73eecc41
Haolinhom <haolinhom@gmail.com>
2020-09-07 23:06:53 +0800

    fix: setting command;

--- commands/setting/index.js
+++ commands/setting/index.js
@@ -3,13 +3,13 @@ const getExistPath = require('../../utils/getExistPath');
 const open = require('open');
 const { std } = require('wu-utils');
 
-function setting() {
+async function setting() {
   if (['-h', '--help'].includes(process.argv[3])) {
     const help = require('./help');
     return std.green.print(' SETTING HELP \r\n', help);
   }
-  const settingPath = path.resolve(__dirname, '../../local/setting.json');
-  if (getExistPath(settingPath)) {
+  const settingPath = await getExistPath(path.resolve(__dirname, '../../local/setting.json'));
+  if (settingPath) {
     open(settingPath);
   }
 }
