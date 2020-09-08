b29db7676abdb5a7fce4c133f816841c6a9aa6bd
HaolinHom <haolinhom@gmail.com>
2020-09-08 16:56:32 +0800

    fix: update dict/common CONFIG;

--- dict/common/CONFIG.js
+++ dict/common/CONFIG.js
@@ -1,6 +1,7 @@
 module.exports = {
   ORGANIZATION: 'wu-node',
-  WU_CLI_CONFIG: 'wu-cli-config.json',
+  PROJECT_CONFIG: 'wu-cli-config.json',
+  PLUGIN_CONFIG: 'wu-cli.json',
   DEFAULT_TEMPLATE: {
     name: 'wu-cli-template',
     repo: 'wu-node/wu-cli-template',
