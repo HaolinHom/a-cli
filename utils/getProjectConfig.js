bb5ec3bd9cae6e84cffd8603fb3ab585f271899b
HaolinHom <haolinhom@gmail.com>
2020-09-08 17:10:37 +0800

    feat: add new utils getProjectConfig;

---
+++ utils/getProjectConfig.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const { std } = require('wu-utils');
+const getExistPath = require('./getExistPath');
+const typeOf = require('./typeOf');
+const CONFIG = require('../dict/common/CONFIG');
+
+function validateConfig(cfg) {
+  if (!cfg.name) {
+    std.error('wu-cli-config.json is a non-compliant file or missing the necessary fields');
+    return false;
+  }
+  return true;
+}
+
+async function getProjectConfig() {
+  const currentPath = process.cwd();
+  const projectCfgPath = await getExistPath(path.resolve(currentPath, CONFIG.PROJECT_CONFIG));
+  if (!projectCfgPath) {
+    return std.error(`Can not find ${CONFIG.PROJECT_CONFIG} in ${currentPath}`);
+  }
+
+  const projectCfg = require(projectCfgPath);
+  if (typeOf(projectCfg) === 'object') {
+    return validateConfig(projectCfg) ? projectCfg : undefined;
+  } else {
+    return std.error(`${CONFIG.PROJECT_CONFIG} is not a json object`);
+  }
+}
+
+module.exports = getProjectConfig;
