7a3d531850aa0c6f30858d99d5b2ad7fb6761e64
HaolinHom <haolinhom@gmail.com>
2020-11-06 17:48:49 +0800

    refactor: cli entry file index.js;

---
+++ cli.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const { Command } = require('commander');
+const { batchRegCommand } = require('wu-utils');
+const chalk = require('chalk');
+const packageJson = require('./package.json');
+const cliCommands = require('./commands');
+
+module.exports = function (extraCommands, actionWrap) {
+	const program = new Command();
+
+	program
+		.name('wucli')
+		.version(packageJson.version)
+		.usage(chalk.greenBright('[command]'))
+		.description(packageJson.description);
+
+	let commandList = cliCommands;
+	if (Array.isArray(extraCommands)) {
+		commandList = commandList.concat(extraCommands);
+	}
+
+	if (typeof actionWrap === 'function') {
+		commandList = commandList.map((item) => ({
+			...item,
+			action: actionWrap(item.action),
+		}));
+	}
+
+	if (cliCommands.length > 0) {
+		batchRegCommand(program, commandList);
+	}
+
+	program.parse(process.argv);
+};
--- index.js
+++ index.js
@@ -2,22 +2,6 @@
 
 'use strict';
 
-const { Command } = require('commander');
-const { batchRegCommand } = require('wu-utils');
-const chalk = require('chalk');
-const packageJson = require('./package.json');
-const cliCommands = require('./commands');
+const cli = require('./cli');
 
-const program = new Command();
-
-program
-	.name('wucli')
-	.version(packageJson.version)
-	.usage(chalk.greenBright('[command]'))
-	.description(packageJson.description);
-
-if (cliCommands.length > 0) {
-	batchRegCommand(program, cliCommands);
-}
-
-program.parse(process.argv);
+cli();
