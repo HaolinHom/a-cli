fb72d42421553259ba0b7b82e021f6ac0d66fd46
HaolinHom <haolinhom@gmail.com>
2020-12-02 17:21:06 +0800

    refactor: dev, build, publish commands, change to call run command instead;

--- commands/build/index.js
+++ commands/build/index.js
@@ -1,14 +1,6 @@
 const run = require('../run');
-const packageInstall = require('../../utils/packageInstall');
 
-function fnBeforeRun(options, config) {
-  if (!options.debug) {
-    packageInstall();
-  }
-}
-
-function build(options) {
-  run('build', options, fnBeforeRun);
-}
-
-module.exports = build;
+module.exports = function (options) {
+  const debugInstallDependencies = true;
+  run('build', options, debugInstallDependencies);
+};
