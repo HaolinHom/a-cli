34ee3dc3308dd4759c982d97b0dd8b033a56856f
HaolinHom <haolinhom@gmail.com>
2021-01-15 16:22:58 +0800

    feat: add default setting file;

---
+++ dict/common/DEFAULT_SETTING.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // Add custom templates to this array
+  templates: [
+    {
+      // template name
+      name: 'a-cli-template',
+      // template repository
+      // To learn more about repo value, visit: https://www.npmjs.com/package/download-git-repo
+      repo: 'a-cli/a-cli-template'
+    },
+  ],
+};
